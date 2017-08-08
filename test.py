21f23b02e36c9b27c9886fd579ffbbdf5c39506a
munmun <m.munmunsharma@gmail.com>
2017-08-08 00:28:18 -0700

    slack assignment1

--- test.py
+++ test.py
@@ -4,12 +4,15 @@ import requests
 umbrellachannel_url = 'https://hooks.slack.com/services/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'
 e = open('umbrella.txt', 'r')
 message = e.readline()
-print message
+
 response = requests.post(
    umbrellachannel_url, data=message,
     headers={'Content-Type': 'application/json'}
 )
-if response.status_code != 200:
+if response.status_code ==200:
+   print response.status_code
+   print response.text
+else:
     raise ValueError(
         'Request failed %s, the response is:\n%s'
         % (response.status_code, response.text)
